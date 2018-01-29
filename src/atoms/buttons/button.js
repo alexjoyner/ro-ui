3b1b866dcbc04209b9c3a32f12b8f1f7824cab00
Alex Joyner <alex@voicir.com>
2018-01-28 22:36:33 -0500

    initialized storybook with a single button component

---
+++ src/atoms/buttons/button.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class Button extends Component {
+    render() {
+        return (
+            <button>{this.props.children}</button>
+        );
+    }
+}
+
+export default Button;
