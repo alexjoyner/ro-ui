02c8d2d3edf20166c16eb95c4834cc42c548ddfb
Alex Joyner <alex@voicir.com>
2018-05-18 10:54:40 -0400

    converted button to styled components

--- src/molecules/sidebar/sidebar.js
+++ src/molecules/sidebar/sidebar.js
@@ -1,8 +1,31 @@
 import React, {Component} from 'react';
 import {Button} from "../../atoms/buttons/buttons";
 import {IoChevronLeft, IoChevronRight} from 'react-icons/lib/io';
-import './sidebar.sass'
+import {Colors} from '../../styles/variables';
+import styled from 'styled-components';
 
+
+const Container = styled.div`
+    transition: 100ms linear;
+    height: 100vh;
+    width: 250px;
+    background-color: ${Colors.primary};
+    color: white;
+    position: fixed;
+    font-size: 20px;
+    padding: 10px;
+    ul{
+        list-style: none;
+    }
+    ${props => (props.shown)? 'left: 0;' : 'left: -270px;'};
+`;
+const SideBarBtn = styled(Button)`
+    margin-left: 0;
+    transition: 100ms linear;
+    position: fixed;
+    ${props => (props.shown)? `left: 270px;`: `left: 0px;`};
+
+`;
 export class SideBar extends Component{
     constructor(props){
         super(props);
@@ -11,22 +34,21 @@ export class SideBar extends Component{
         }
     }
     toggleSidebar() {
-        console.log(this.state.shown)
         this.setState({
             shown: !this.state.shown
         })
     }
     render() {
         return (
-            <div className={`sidebar-container ${(this.state.shown)? 'sidebar-shown': 'sidebar-hidden'}`}>
-                <Button color={'primary'} className={'sidebar-btn'} onClick={() => this.toggleSidebar(null)}>
+            <Container  {...this.state}>
+                <SideBarBtn {...this.state} primary onClick={() => this.toggleSidebar(null)}>
                     {(this.state.shown)?
                         <IoChevronLeft style={{'textAlign': 'center'}}/>:
                         <IoChevronRight style={{'textAlign': 'center'}}/>
                     }
-                </Button>
+                </SideBarBtn>
                 {this.props.children}
-            </div>
+            </Container>
         )
     }
 };
