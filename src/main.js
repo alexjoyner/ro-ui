6b1ed79f3e52f6576723186a1051eba29fccc34d
Alex Joyner <alex@voicir.com>
2018-06-05 14:58:41 -0400

    fixed lintings for main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
-import {injectGlobal} from 'styled-components';
-injectGlobal`
+import { injectGlobal } from 'styled-components';
+/*
+============ Atoms ===========
+ */
+import { Button } from './atoms/Button';
+import { colors } from './atoms/colors';
+import { Graph } from './atoms/Graph';
+import { ImageOverlay } from './atoms/ImageOverlay';
+import { Input } from './atoms/Input';
+import { Logo } from './atoms/Logo';
+import { Panel } from './atoms/Panel';
+import { ProfilePic } from './atoms/ProfilePic';
+import { TopBar } from './atoms/TopBar';
+import { CenteredContent } from './atoms/CenteredContent';
+import { PlainBox } from './atoms/PlainBox';
+
+/*
+============ Molecules ===========
+ */
+import { HeroImage } from './molecules/HeroImage';
+import { SideBar } from './molecules/SideBar';
+import { GaugeBlock } from './molecules/GaugeBlock';
+import { Header } from './molecules/Header';
+import { Modal } from './molecules/Modal';
+
+injectGlobal` 
   @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400');
   body {
     padding: 0;
@@ -10,43 +34,35 @@ injectGlobal`
     font-weight: 100;
   }
 `;
-/* 
-============ Atoms ===========
- */
-import {Button} from './atoms/Button';
-import {colors} from './atoms/colors';
-import {Graph} from './atoms/Graph';
-import {Gauge} from './atoms/Gauge';
-import {Grid} from './atoms/Grid';
-import {ImageOverlay} from './atoms/ImageOverlay';
-import {Input} from './atoms/Input';
-import {Logo} from './atoms/Logo';
-import {Panel} from './atoms/Panel';
-import {ProfilePic} from './atoms/ProfilePic';
-import {TopBar} from './atoms/TopBar';
-import {CenteredContent} from './atoms/CenteredContent';
-import {PlainBox} from './atoms/PlainBox';
+
 // Export Atoms
-export {Button, colors, Graph, Grid, ImageOverlay, Input, Logo, Panel, ProfilePic, TopBar, CenteredContent, PlainBox}
-
-/* 
-============ Molecules ===========
- */
-import {HeroImage} from './molecules/HeroImage';
-import {SideBar} from './molecules/SideBar';
-import {GaugeBlock} from './molecules/GaugeBlock';
-import {Header} from './molecules/Header'; 
-import {Modal} from './molecules/Modal'; 
+export {
+  Button,
+  colors,
+  Graph,
+  ImageOverlay,
+  Input,
+  Logo,
+  Panel,
+  ProfilePic,
+  TopBar,
+  CenteredContent,
+  PlainBox,
+};
 // Export Molecules
-export {HeroImage, SideBar, GaugeBlock, Header, Modal};
+export {
+  HeroImage,
+  SideBar,
+  GaugeBlock,
+  Header,
+  Modal,
+};
 
-/* 
-
+/*
 After this point, components should be project specific.
     - organisms: putting together molecules and atoms
     - templates: quick sketches on paper that include where organisms will be
     - pages: coded versions of the templates with dummy text
     - release product
 */
-
 
