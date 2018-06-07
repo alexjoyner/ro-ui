113cc0d74dce594253e4cb0d3574f1e76661c8cd
Alex Joyner <alex@voicir.com>
2018-06-07 08:16:19 -0400

    added tests for gauge block and error boundary

--- src/molecules/GaugeBlock/test.js
+++ src/molecules/GaugeBlock/test.js
@@ -5,12 +5,15 @@ import { GaugeBlock } from './';
 describe('GaugeBlock component', () => {
   let component;
   beforeEach(() => {
-    component = shallow(<GaugeBlock />);
+    component = shallow(<GaugeBlock label="Test Sensor" />);
   });
   it('Should render without exploding', () => {
     expect(component).toBeDefined();
   });
   it('Should match snapshot', () => {
     expect(component).toMatchSnapshot();
+  });
+  it('Should display the label', () => {
+    expect(component.contains('Test Sensor')).toBe(true);
   });
 });
