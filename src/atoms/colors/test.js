543fd4597906a647b5204248c685b9f002722768
Rosco <rosco@Roscos-MacBook-Pro-2.local>
2018-06-19 11:00:53 -0400

    updated page overlay styles and colors test

--- src/atoms/colors/test.js
+++ src/atoms/colors/test.js
@@ -2,17 +2,6 @@ import { colors } from './';
 
 describe('colors', () => {
   it('Should match all default colors', () => {
-    expect(colors).toEqual({
-      primaryLight: '#74b9ff',
-      primary: '#0984e3',
-      successLight: '#55efc4',
-      success: '#00b894',
-      warningLight: '#ffeaa7',
-      warning: '#fdcb6e',
-      dangerLight: '#ff7675',
-      danger: '#d63031',
-      dark: '#2d3436',
-      darkLight: '#636e72',
-    });
+    expect(colors).toMatchSnapshot();
   });
 });
