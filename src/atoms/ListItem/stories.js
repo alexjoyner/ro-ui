76e730a6e2921cc497076674d017c537fc6ed02b
alexjoyner <rosco9awj@gmail.com>
2018-09-24 15:35:51 -0400

    fixed list stylings and linked all props to add support for click events

---
+++ src/atoms/ListItem/stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ListItem } from './';
+import { Atoms } from '../_atoms.stories';
+import { colors } from '../colors';
+
+
+Atoms
+  .add('ListItem', () => (
+    <ul>
+      <ListItem color={colors.primaryLight}>Test Item</ListItem>
+    </ul>
+  ));
