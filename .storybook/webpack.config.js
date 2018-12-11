2e643480bcda801cb7c4f4b0cdce9c9492ab83cf
Alex Joyner <alex@voicir.com>
2018-12-11 00:10:34 -0500

    updated to latest ro-component-library from macbook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        loaders: ["style-loader", "css-loader"],
+        include: [path.resolve(__dirname, "../")]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loaders: ["file-loader"]
+      }
+    ]
+  }
+};
