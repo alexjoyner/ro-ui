e9c6a1b84da2af4334a972db445376eede615ffd
alexjoyner <rosco9awj@gmail.com>
2019-01-14 10:16:30 -0500

    added overrides to button component

--- src/atoms/Button/index.js
+++ src/atoms/Button/index.js
@@ -2,21 +2,30 @@ import React from 'react';
 import { ButtonWrapper } from './ButtonWrapper';
 import { ButtonStyles } from './ButtonStyles';
 
-const Button = ({
-  color, size, ghost, children, ...props
-}) => (
-  <ButtonWrapper {...props} type="button">
-    {/* Passing props to Button Styles seperately to fix double onClick events from button */}
-    <ButtonStyles
-      color={color}
-      size={size}
-      ghost={ghost}
-      tabIndex={-1}
-    >
-      {children}
-    </ButtonStyles>
-  </ButtonWrapper>
-);
+const mergePropsWithOverrides = (props) => {
+  const overrides = props.overrides || {};
+  return { ...props, ...overrides[props.name] } || {};
+};
+
+const Button = (props) => {
+  const sharedProps = mergePropsWithOverrides(props);
+  const {
+    color, size, ghost, children, ...restProps
+  } = sharedProps;
+  return (
+    <ButtonWrapper {...restProps} type="button">
+      {/* Passing props to Button Styles seperately to fix double onClick events from button */}
+      <ButtonStyles
+        color={color}
+        size={size}
+        ghost={ghost}
+        tabIndex={-1}
+      >
+        {children}
+      </ButtonStyles>
+    </ButtonWrapper>
+  );
+};
 
 Button.defaultProps = {
   color: 'default',
