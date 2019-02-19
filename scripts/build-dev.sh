9affa8d2f098d49f220eb1105510e0822cf2fa53
Alex Joyner <alex@voicir.com>
2019-02-19 14:27:59 -0500

    switched publish strategy to use only whats in dist folder

---
+++ scripts/build-dev.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+mkdir -p dist
+cp README.md dist/README.md
+cp package.json dist/package.json
+cp -r styles dist/styles
+babel src/ --out-dir dist --ignore **/setupTests.js,**/test.js,**/*.test.js,**/*.stories.js,**/stories.js
+
+pushd ./dist
+yarn link
+popd
