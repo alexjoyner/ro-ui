fa469beddaaaa2c97dd909e70fbd977b688b19f9
alexjoyner <rosco9awj@gmail.com>
2019-02-26 14:23:05 -0500

    fixed broken path and re-added original typography styles

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -3,7 +3,7 @@
 mkdir -p dist
 cp README.md dist/README.md
 cp package.json dist/package.json
-cp -r styles dist/styles
+cp -r src/styles dist/styles
 babel src/ --out-dir dist --ignore **/setupTests.js,**/test.js,**/*.test.js,**/*.stories.js,**/stories.js
 
 pushd ./dist
