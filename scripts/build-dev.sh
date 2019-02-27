3a81b001e6ae6cce57f1677ea24ddd299bae4ae7
Alex Joyner <alex@voicir.com>
2019-02-27 11:22:49 -0500

    exported styled components

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -4,6 +4,7 @@ mkdir -p dist
 cp README.md dist/README.md
 cp package.json dist/package.json
 cp -r src/styles dist/styles
+cp -r src/styles dist
 babel src/ --out-dir dist --ignore **/setupTests.js,**/test.js,**/*.test.js,**/*.stories.js,**/stories.js
 
 pushd ./dist
