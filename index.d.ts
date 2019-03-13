ce5333183ec8d4c638d972ad2c9ee28f51b178cb
Alex Joyner <alex@voicir.com>
2019-03-13 01:11:29 -0400

    added types for typescript support

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+declare module 'ro-component-library/Styletron';
+declare module 'ro-component-library/Themes';
+declare module 'ro-component-library/Toast';
--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -3,6 +3,7 @@
 mkdir -p dist
 cp README.md dist/README.md
 cp package.json dist/package.json
+cp index.d.ts dist/index.d.ts
 cp -r src/styles dist/styles
 cp -r src/styles dist
 babel src/ --out-dir dist --ignore **/setupTests.js,**/test.js,**/*.test.js,**/*.stories.js,**/stories.js
