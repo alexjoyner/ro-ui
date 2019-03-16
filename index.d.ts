7085ceaf78e97218a6ff14019797ccf75e42ab60
Alex Joyner <alex@voicir.com>
2019-03-16 16:35:31 -0400

    added module declarations for types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
 declare module 'ro-component-library/Styletron';
 declare module 'ro-component-library/Themes';
 declare module 'ro-component-library/Toast';
+declare module 'ro-component-library/Button';
+declare module 'ro-component-library/Input';
+declare module 'ro-component-library/Block';
+declare module 'ro-component-library/Modal';
+declare module 'ro-component-library/Card';
