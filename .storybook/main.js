5df988090eeb3a483c4e8762460d1b27d0b6a35c
Alex Joyner <alex@voicir.com>
2020-06-20 11:58:47 -0400

    edited storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-	stories: ['../packages/**/src/{**/,}*.stories.tsx'],
+	stories: ['../packages/**/src/{**/,}*.stories.tsx', '../apps/**/src/{**/,}*.stories.{t,j}sx'],
 	addons: ['@storybook/preset-typescript'],
 };
