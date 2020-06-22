5f46245f0d279c8001853401840dc3ed5c9c8661
Alex Joyner <alex@voicir.com>
2020-06-21 20:14:31 -0400

    updated card components and stories

--- packages/ro-ui-atoms/src/Card/Card.stories.tsx
+++ packages/ro-ui-atoms/src/Card/Card.stories.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
 import { Card } from './';
+import { Button } from '../Button';
 
 export default { title: 'ro-ui-atoms/Card' }
 
 export const Basic = () => (
 	<Card>Hello World</Card>
+)
+export const WithSubComponents = () => (
+	<Card>
+		{({ Header, Footer, Body }) => (
+			<>
+				<Header>Header</Header>
+				<Body>Body Contents</Body>
+				<Footer>Footer Contents</Footer>
+			</>
+		)}
+	</Card>
+)
+
+export const WithAlignedItems = () => (
+	<Card>
+		{({ Header, Footer, Body }) => (
+			<>
+				<Header>
+					<Button style={{ marginRight: 'auto' }}>Left</Button>
+					<Button>Center</Button>
+					<Button style={{ marginLeft: 'auto' }}>Right</Button>
+				</Header>
+				<Body>Body Contents</Body>
+				<Footer>
+					<Button style={{ marginRight: 'auto' }}>Left</Button>
+					<Button>Center</Button>
+					<Button style={{ marginLeft: 'auto' }}>Right</Button>
+				</Footer>
+			</>
+		)}
+	</Card>
 )
--- packages/ro-ui-atoms/src/Card/index.tsx
+++ packages/ro-ui-atoms/src/Card/index.tsx
@@ -1,11 +1,53 @@
+import React from 'react';
 import styled from 'styled-components';
 
-const Card = styled.div`
-display: block;
-width: 300px;
+const CardBorderRadius = '5px'
+
+const Container = styled.div`
+display: grid;
+& > * {
+	padding: 10px;
+};
 height: 300px;
-border: 1px solid grey;
-border-radius: 5px;
+width: 300px;
+box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+border-radius: ${CardBorderRadius};
 `;
+const Header = styled.div`
+grid-row-start: 1;
+grid-row-end: 2;
+display: flex;
+flex-direction: row;
+align-items: center;
+border-radius: ${CardBorderRadius} ${CardBorderRadius} 0px 0px;
+`
+const Body = styled.div`
+grid-row-start: 2;
+grid-row-end: 9;
+`
+const Footer = styled.div`
+grid-row-start: 9;
+grid-row-end: 10;
+display: flex;
+align-items: center;
+border-radius: 0px 0px ${CardBorderRadius} ${CardBorderRadius};
+`
+const Card = ({ children, ...props }) => {
+	const renderChildren = () => {
+		if (typeof children === 'function') {
+			return children({
+				Header,
+				Body,
+				Footer
+			})
+		}
+		return children;
+	}
+	return (
+		<Container {...props}>
+			{renderChildren()}
+		</Container>
+	)
+}
 
 export { Card };
