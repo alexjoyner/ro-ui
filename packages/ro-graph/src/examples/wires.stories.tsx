37fee20c679624c8201750db9bff902071f666a9
alexjoyner <rosco9awj@gmail.com>
2020-07-29 18:56:40 -0400

    added 8_Pin_IC_Part
    Signed-off-by: alexjoyner <rosco9awj@gmail.com>

--- packages/ro-graph/src/examples/wires.stories.tsx
+++ packages/ro-graph/src/examples/wires.stories.tsx
@@ -3,21 +3,27 @@ import { makeGraph, update } from '../utils/makeGraph';
 import { applyWireStyles } from '../utils/styleGraph';
 import { registerShape } from '../utils/registerShape';
 import { MxResistor } from '../circuit/MxResistor';
+import { makeMX_8PinIC } from '../circuit/Mx8PinIC';
 import './styles/wire-styles.css';
 
 export default { title: 'ro-graph/Wires' };
 
 export const BasicWires = () => {
 	useEffect(() => {
-		const fillColor = '#FFFFFF';
 		const container = document.getElementById('HelloWorldExample')
 		const { graph, parent, mxApi } = makeGraph({}, container);
+
+		graph.view.scale = 1;
+		graph.setPanning(true);
+		graph.setConnectable(true);
+		graph.setConnectableEdges(true);
+		graph.setDisconnectOnMove(false);
+		graph.foldingEnabled = false;
+
 		applyWireStyles(graph, { grid: true });
 		registerShape(mxApi, 'resistor', MxResistor);
 		update(graph, () => {
-			const v1 = graph.insertVertex(parent, null, 'J1', 80, 40, 40, 80,
-				`verticalLabelPosition=top;verticalAlign=bottom;shadow=1;fillColor=${fillColor}`);
-			v1.setConnectable(true);
+			makeMX_8PinIC(graph, parent, {});
 			var v2 = graph.insertVertex(parent, null, 'R1', 220, 220, 80, 20,
 				'shape=resistor;verticalLabelPosition=top;verticalAlign=bottom;');
 		})
