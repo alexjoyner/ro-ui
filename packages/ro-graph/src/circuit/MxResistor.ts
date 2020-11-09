5c6d810b26f5ce233f2f93763faf04d8ceec3ebe
alexjoyner <rosco9awj@gmail.com>
2020-11-08 21:28:08 -0500

    fixed bug with displaying resistor
    Signed-off-by: alexjoyner <rosco9awj@gmail.com>

--- packages/ro-graph/src/circuit/MxResistor.ts
+++ packages/ro-graph/src/circuit/MxResistor.ts
@@ -1,6 +1,7 @@
 import mx from 'mxgraph';
 const {
 	mxCylinder,
+	mxPoint
 } = mx();
 
 function MxResistor() { }
@@ -8,7 +9,7 @@ MxResistor.prototype = new mxCylinder();
 MxResistor.prototype.constructor = MxResistor;
 
 MxResistor.prototype.redrawPath = function (path, x, y, w, h, isForeground) {
-	var dx = w / 16;
+	const dx = w / 16;
 
 	if (isForeground) {
 		path.moveTo(0, h / 2);
@@ -25,4 +26,41 @@ MxResistor.prototype.redrawPath = function (path, x, y, w, h, isForeground) {
 		path.end();
 	}
 }
-export { MxResistor };
+
+const makeMX_Resistor = (graph, parent, options) => {
+  const opts = {
+    label: 'R1',
+    fillColor: '#FFFFFF',
+    fontColor: '#000000',
+    strokeColor: '#000000',
+    connectable: true,
+    x: 220,
+    y: 220,
+    ...options
+  }
+  
+	const v0 = graph.insertVertex(parent, null, 'R1', opts.x, opts.y, 0, 0,
+	'shape=square;verticalLabelPosition=top;verticalAlign=bottom;strokeColor=transparent;');
+  v0.setConnectable(false);
+
+	const v1 = graph.insertVertex(v0, null, null, 0, 0, 80, 20,
+  'shape=resistor;verticalLabelPosition=top;verticalAlign=bottom;');
+  v1.setConnectable(false);
+  v1.geometry.relative = true;
+
+  const v11 = graph.insertVertex(v1, null, null, 0, 0, 10, 16,
+      'shape=line;align=left;verticalAlign=middle;fontSize=10;routingCenterX=-0.5;'+
+      'spacingLeft=12;fontColor=' + opts.fontColor + ';strokeColor=' + opts.strokeColor);
+  v11.geometry.relative = true;
+  v11.geometry.offset = new mxPoint(-v11.geometry.width, 2);
+
+  const v15 = v11.clone();
+  v15.geometry.x = 1;
+  v15.style =	'shape=line;align=right;verticalAlign=middle;fontSize=10;routingCenterX=0.5;'+
+    'spacingRight=12;fontColor=' + opts.fontColor + ';strokeColor=' + opts.strokeColor;
+  v15.geometry.offset = new mxPoint(0, 2);
+  v1.insert(v15);
+}
+
+
+export { MxResistor, makeMX_Resistor };
--- packages/ro-graph/src/examples/wiresExample.tsx
+++ packages/ro-graph/src/examples/wiresExample.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { makeGraph, update } from '../utils/makeGraph';
 import { applyWireStyles } from '../utils/styleGraph';
 import { registerShape } from '../utils/registerShape';
-import { MxResistor } from '../circuit/MxResistor';
+import { MxResistor, makeMX_Resistor } from '../circuit/MxResistor';
 import { makeMX_8PinIC } from '../circuit/Mx8PinIC';
 import { withWireConnections } from '../circuit/withWireConnections';
 import { setupCircuitBuider } from '../circuit/setupCircuitBuilder';
@@ -14,21 +14,18 @@ export const BasicWires = () => {
 	useEffect(() => {
 		const container = document.getElementById('HelloWorldExample')
 		const { graph, parent, mxApi } = makeGraph({}, container);
-		
 		setupCircuitBuider(graph, mxApi);
 		setupKeyBindings(graph, mxApi);
 		withWireConnections(mxApi);
 		applyWireStyles(graph, { grid: true });
 		registerShape(mxApi, 'resistor', MxResistor);
 		update(graph, () => {
-			var v2 = graph.insertVertex(parent, null, 'R1', 220, 220, 80, 20,
-				'shape=resistor;verticalLabelPosition=top;verticalAlign=bottom;');
-			var v3 = graph.insertVertex(parent, null, 'R2', 120, 120, 80, 20,
-				'shape=resistor;verticalLabelPosition=top;verticalAlign=bottom;');
+			makeMX_8PinIC(graph, parent, {});
+			makeMX_Resistor(graph, parent, {});
 		})
 			
 		// Starts connections on the background in wire-mode
-		var connectionHandlerIsStartEvent = graph.connectionHandler.isStartEvent;
+		const connectionHandlerIsStartEvent = graph.connectionHandler.isStartEvent;
 		graph.connectionHandler.isStartEvent = function(me)
 		{
 			return connectionHandlerIsStartEvent.apply(this, arguments);
